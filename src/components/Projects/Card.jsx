9ceee18b6446193bbe9faf61455bfd6cc5223838
mir-asif01 <asifbsb2002@gmail.com>
2024-06-21 17:36:35 +0600

    project data added

--- src/components/Projects/Card.jsx
+++ src/components/Projects/Card.jsx
@@ -1,15 +1,15 @@
 import { FaGlobe, FaGithub } from "react-icons/fa6";
-import bannerImg from "../../assets/banner.jpg"
-function Card() {
+function Card({ prj }) {
+    const { img, name, code, live } = prj
     return <>
-        <div className="relative">
-            <img src={bannerImg} className="w-[2000px]" alt="" />
-            <p className="text-center font-bold">PROJECT NAME</p>
-            <div className="absolute opacity-0 hover:opacity-100 bottom-0 left-0 w-full h-full flex justify-center items-center transition-all duration-200 gap-6">
-                <a className="cursor-pointer text-3xl text-slate-700 bg-gray-200 rounded-full p-3">
+        <div className="relative p-4 border border-slate-400">
+            <img src={img} className="border border-gray-300 rounded-md" alt="" />
+            <p className="mt-7 text-center text-slate-700 text-xl font-semibold">{name}</p>
+            <div className="absolute opacity-0 hover:opacity-100 bottom-0 left-0 w-full h-full flex justify-center items-center transition-all duration-200 gap-6 bg-black/10">
+                <a target="_blank" href={code} className="cursor-pointer text-3xl bg-slate-800 rounded-full p-3 text-white transition-all duration-200">
                     <FaGithub />
                 </a>
-                <a className="cursor-pointer text-3xl text-slate-700 bg-gray-200 rounded-full p-3">
+                <a target="_blank" href={live} className="cursor-pointer text-3xl bg-slate-800 rounded-full p-3 text-white transition-all duration-200">
                     <FaGlobe />
                 </a>
             </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,41 @@
 import Card from "./Card"
-
+import p1 from "../../assets/p1.png"
+import p2 from "../../assets/p2.png"
+import p3 from "../../assets/p3.png"
+import p4 from "../../assets/p4.png"
 function Projects() {
+
+    const projectsData = [
+        {
+            id: 1,
+            name: "E REPAIR SHOP",
+            img: p1,
+            code: "https://github.com/mir-asif01/e_repair_shop-client",
+            live: "https://recipe-app-4be44.web.app/"
+        },
+        {
+            id: 2,
+            name: "PRO TASK",
+            img: p2,
+            code: "https://github.com/mir-asif01/pro_task_frontend",
+            live: "https://pro-task-ed52c.web.app/"
+        },
+        {
+            id: 3,
+            name: "PROJECTILE",
+            img: p3,
+            code: "https://github.com/mir-asif01/projectile-frontend",
+            live: "https://projectileas.netlify.app/"
+        }
+    ]
+
     return <>
         <section className="p-10 sm:p-32">
-            <h1 className="text-4xl text-slate-700 font-bold">PROJECTS</h1>
+            <h1 className="text-4xl text-slate-700 font-bold">TOP PROJECTS</h1>
             <div className="mt-10 flex flex-col sm:flex-row justify-center items-center gap-10">
-                <Card />
-                <Card />
-                <Card />
+                {
+                    projectsData.map(prj => <Card key={prj.id} prj={prj} />)
+                }
             </div>
         </section>
     </>
