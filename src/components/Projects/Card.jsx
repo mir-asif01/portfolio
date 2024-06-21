db517a850d1c078ede603e4372bfc9f5e20c48f8
mir-asif01 <asifbsb2002@gmail.com>
2024-06-21 18:07:24 +0600

    favicon and title added

--- src/components/Projects/Card.jsx
+++ src/components/Projects/Card.jsx
@@ -2,7 +2,7 @@ import { FaGlobe, FaGithub } from "react-icons/fa6";
 function Card({ prj }) {
     const { img, name, code, live } = prj
     return <>
-        <div className="relative p-4 border border-slate-400">
+        <div className="relative p-4 border rounded-md border-slate-400">
             <img src={img} className="border border-gray-300 rounded-md" alt="" />
             <p className="mt-7 text-center text-slate-700 text-xl font-semibold">{name}</p>
             <div className="absolute opacity-0 hover:opacity-100 bottom-0 left-0 w-full h-full flex justify-center items-center transition-all duration-200 gap-6 bg-black/10">
