5aa765df891500388af1dfebae4af5f649beae7b
mir-asif01 <asifbsb2002@gmail.com>
2024-07-07 17:44:31 +0600

    responsive added added in the homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,9 +4,13 @@ import { FaGithub, FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function Home() {
   return (
-    <section id="home" className="flex justify-center items-center">
-      <div className="p-40 w-2/3 flex flex-col items-center justify-center text-center">
-        <img src={bannerImg} className="rounded-full h-52 w-52" alt="" />
+    <section id="home" className="flex justify-center items-center p-3 md:p-40">
+      <div className="md:w-2/3 flex flex-col items-center justify-center text-center">
+        <img
+          src={bannerImg}
+          className="rounded-full p-2 md:h-52 md:w-52"
+          alt=""
+        />
         <h1 className="text-2xl sm:text-4xl sm:mb-4 text-slate-700">
           Mir Kamrul Ahsan Asif
         </h1>
